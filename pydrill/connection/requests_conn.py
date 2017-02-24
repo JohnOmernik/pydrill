085a2406eb720642557c92a5407227e186e52ac2
johnomernik <john@omernik.com>
2017-02-24 14:04:33 -0600

    Added Drill Auth

--- pydrill/connection/requests_conn.py
+++ pydrill/connection/requests_conn.py
@@ -53,9 +53,65 @@ class RequestsHttpConnection(Connection):
                 raise ImproperlyConfigured("You cannot pass CA certificates when verify SSL is off.")
             self.session.verify = ca_certs
 
+        if drill_auth is not None:
+            self.drill_user = drill_auth.split(':')[0]
+            self.drill_pass = drill_auth.split(':')[1]
+            self.perform_auth()
+
         if use_ssl and not verify_certs:
             warnings.warn('Connecting to %s using SSL with verify_certs=False is insecure.' % self.base_url)
 
+    def perform_auth(self, timeout=None):
+        url = self.base_url + "/j_security_check"
+
+        if timeout is None:
+            timeout = self.timeout
+
+        login = {'j_username': self.drill_user, 'j_password': self.drill_pass}
+        print(login)
+        method = 'POST'
+        body = "Auth Request not shown for security"
+
+        start = time.time()
+
+        try:
+            response = self.session.request(
+                method,
+                url,
+                data=login,
+#                headers={'Content-Type': 'application/json'},
+                timeout=timeout,
+            )
+
+            response.encoding = 'UTF-8'
+            duration = time.time() - start
+            raw_data = response.text
+        except requests.exceptions.SSLError as e:
+            self.log_request_fail(method, url, None, time.time() - start, exception=e)
+            raise SSLError('N/A', str(e), e)
+        except requests.Timeout as e:
+            self.log_request_fail(method, url, None, time.time() - start, exception=e)
+            raise ConnectionTimeout('TIMEOUT', str(e), e)
+        except requests.ConnectionError as e:
+            self.log_request_fail(method, url, None, time.time() - start, exception=e)
+            raise ConnectionError('N/A', str(e), e)
+
+        # raise errors based on http status codes, let the client handle those if need
+        if not (200 <= response.status_code < 300) and response.status_code not in ignore:
+            self.log_request_fail(method, url, None, duration, response.status_code)
+            self._raise_error(response.status_code, "Status Code Error:\n" + raw_data)
+
+        if raw_data.find("Invalid username/password credentials") >= 0:
+            self.log_request_fail(method, url, None, duration, response.status_code)
+            self._raise_error(response.status_code, "Auth Failure:\n" + raw_data)
+
+        if raw_data.find("Number of Drill Bits") == 0:
+            self.log_request_fail(method, url, None, duration, response.status_code)
+            self._raise_error(response.status_code, "Unknown Error:\n" + raw_data)
+
+        self.log_request_success(method, url, response.request.path_url, None, response.status_code, raw_data, duration)
+
+        return response, raw_data, duration
     def perform_request(self, method, url, params=None, body=None, timeout=None, ignore=()):
         url = self.base_url + url
         if params:
